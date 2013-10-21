87b1127f578dd18344db1bfa5923962dbbcb8258
Park Hyunwoo <ez@smartstudy.co.kr>
2013-10-21 23:22:18 +0900

    Parsing node data from string

--- logiform.js
+++ logiform.js
@@ -2,13 +2,13 @@
 
     $.logiform = function(element, options) {
         var defaults = {
-            'data': "{ $and: [] }",
+            'hide_original': true,
+            'data': '{"$and":[]}',
             'operators': {
                 'logical': [
                     {'expression': '$and', 'description': 'AND'},
                     {'expression': '$or', 'description': 'OR'},
                     {'expression': '|'},
-                    {'expression': '$not', 'description': 'NOT'},
                     {'expression': '$nor', 'description': 'NOR'}
                 ],
                 'comparison': [
@@ -29,9 +29,12 @@
         var logiform = this;
         logiform.settings = {}
 
+        var condition_group_mockup;
+        var condition_group;
+        var fieldValueMockup = {};
+
         var $element = $(element),
              element = element;
-        var root;
 
         logiform.init = function() {
             logiform.settings = $.extend({}, defaults, options);
@@ -72,11 +75,10 @@
                 '</select>';
 
             // Prepare field items and its mockup element for value.
-            var fieldValueMockup = {};
             var firstFieldValueMockup = '';
             var fieldItems = '';
-            for (var idxSchema = 0, szSchema = logiform.settings.schema.length; idxSchema < szSchema; idxSchema++) {
-                var item = logiform.settings.schema[idxSchema];
+            for (var i = 0, sz = logiform.settings.schema.length; i < sz; i++) {
+                item = logiform.settings.schema[i];
 
                 if (item.id == '|') {
                     fieldItems += divider
@@ -110,7 +112,7 @@
                 '</select>';
 
             // Create a mockup for condition
-            var condition_mockup = 
+            condition_mockup = 
                 '<div class="lf-condition">' +
                     fieldContent +
                     comparisonOperatorContent + 
@@ -121,7 +123,7 @@
                 '</div>';
 
             // Create a mockup for condition group
-            var condition_group_mockup = 
+            condition_group_mockup = 
                 '<div class="lf-condition-group">' +
                     logicalOperatorContent +
                     '<button type="button" class="lf-button-remove-condition-group">Remove</button>' +
@@ -133,51 +135,129 @@
                     '</div>' +
                 '</div>';
 
-            // Join all
-            root = $(condition_group_mockup).attr('id', 'lf-root');
+            // Create root
+            var root = $(condition_group_mockup).attr('id', 'lf-root');
             root.find('.lf-button-remove-condition-group').attr('disabled', 'disabled');
-
-            // Hide source
-            //$element.hide();
-
-            // Append to parent
-            $element.after(root);
 
             // Set up handlers
             root.on('click', '.lf-button-add-condition', function() {
                 $(this).parents('.lf-condition-group:first').find('.lf-condition-list:first').append($(condition_mockup));
-                logiform.update();
+                logiform.bake(root);
             });
             root.on('click', '.lf-button-add-condition-group', function() {
                 $(this).parents('.lf-condition-group:first').find('.lf-condition-list:first').append($(condition_group_mockup));
-                logiform.update();
+                logiform.bake(root);
             });
             root.on('click', '.lf-button-remove-condition', function() {
                 $(this).parents('.lf-condition:first').remove();
-                logiform.update();
+                logiform.bake(root);
             });
             root.on('click', '.lf-button-remove-condition-group', function() {
                 var group = $(this).parents('.lf-condition-group:first');
                 // Do not remove root element
                 if (group.attr('id') == 'lf-root') return;
                 group.remove();
-                logiform.update();
+                logiform.bake(root);
             });
             root.on('change', '.lf-field', function() {
                 $(this).siblings('.lf-condition-value').html(fieldValueMockup[$(this).val()]);
-                logiform.update();
+                logiform.bake(root);
             });
             root.on('change', '.lf-comparisonoperator, .lf-logicaloperator, .lf-value', function() {
-                logiform.update();
+                logiform.bake(root);
             });
+
+            // Parse data if exists
+            if (logiform.settings.data) {
+                logiform.parse(JSON.parse(logiform.settings.data), root);
+            }
+
+            // Hide source
+            if (logiform.settings.hide_original) {
+                $element.hide();
+            }
+
+            // Append to parent
+            $element.after(root);
+
         }
 
-        logiform.update = function() {
+        logiform.parse = function(tree, node) {
             // Traversing condition tree
-            $element.val(JSON.stringify(logiform._traverse(root)));
+            for (var logicalOperator in tree) {
+
+                // Set logical operator
+                for (var l = 0, szl = logiform.settings.operators.logical.length; l < szl; l++) {
+                    item = logiform.settings.operators.logical[l];
+                    if (item['expression'] == logicalOperator) {
+                        node.find('.lf-logicaloperator').val(logicalOperator);
+                        break;
+                    }
+                }
+
+                // Add conditions
+                var conditions = node.find('.lf-condition-list');
+                for (var i = 0, sz = tree[logicalOperator].length; i < sz; i++) {
+                    condition = tree[logicalOperator][i];
+
+                    for (var field in condition) {
+
+                        // Check whether field name is in scheme,
+                        for (var s = 0, szs = logiform.settings.schema.length; s < szs; s++) {
+                            item = logiform.settings.schema[s];
+
+                            if (item['id'] == field) {
+                                // Create condition and append to list
+                                var condition_node = $(condition_mockup);
+                                condition_node.find('.lf-field').val(field);
+                                condition_node.find('.lf-condition-value').html(fieldValueMockup[field]);
+                                conditions.append(condition_node);
+
+                                // Set value for it
+                                for (var comparisonOperator in condition[field]) {
+                                    var value = condition[field][comparisonOperator];
+                                    condition_node.find('.lf-comparisonoperator').val(comparisonOperator);
+                                    // TODO: Multiple select box
+                                    condition_node.find('.lf-value').val(value);
+                                }
+                                // Break here, only one value per a statement.
+                                break;
+                            }
+                        }
+
+                        // ...or, this is a nested condition group.
+                        for (var l = 0, szl = logiform.settings.operators.logical.length; l < szl; l++) {
+                            item = logiform.settings.operators.logical[l];
+
+                            if (item['expression'] == field) {
+                                // Create condition group and append to list
+                                var condition_group_node = $(condition_group_mockup);
+
+                                // Initiate condition group
+                                logiform.parse(condition, condition_group_node);
+
+                                conditions.append(condition_group_node);
+                            }
+                        }
+                        // Break here, only one statement per a condition.
+                        break;
+                    }
+                }
+                // Break here, only one logical opeartor per a group.
+                break;
+            }
+
         }
 
-        logiform._traverse = function(node) {
+        logiform._traverse_parse = function(node) {
+        }
+
+        logiform.bake = function(node) {
+            // Traversing condition tree
+            $element.val(JSON.stringify(logiform._traverse_bake(node)));
+        }
+
+        logiform._traverse_bake = function(node) {
             var form = {};
             var logicalOperator = node.children('.lf-logicaloperator:first').val();
             var conditions = [];
@@ -185,7 +265,7 @@
                 var cond = $(this);
 
                 if (cond.hasClass('lf-condition-group')) {
-                    conditions.push(logiform._traverse(cond));
+                    conditions.push(logiform._traverse_bake(cond));
                 } else if (cond.hasClass('lf-condition')) {
                     var field = cond.find('.lf-field').val();
                     var comparisonOperator = cond.find('.lf-comparisonoperator').val();
